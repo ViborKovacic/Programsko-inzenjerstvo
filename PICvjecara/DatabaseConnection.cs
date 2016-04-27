30ead984004f542e79793693fa20db544f0c6ebf
ViborKovacic <vibor.kovacic@gmail.com>
2016-04-27 19:33:40 +0200

    Dodana klasa za spajanje na bazu

---
+++ PICvjecara/DatabaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace PICvjecara
+{
+    class DatabaseConnection
+    {
+        public static SqlConnection conn = null;
+
+        public void Connection()
+        {
+            conn = new SqlConnection("Data Source=31.147.204.119\\PISERVER,1433;Initial Catalog=16027_DB;Persist Security Info=True;User ID=16027_User;Password=***********");
+            conn.Open();
+        }
+    }
+}
--- PICvjecara/UnosArtikla.cs
+++ PICvjecara/UnosArtikla.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PICvjecara
 {
@@ -37,6 +38,24 @@ namespace PICvjecara
         {
             /* otvoriti konekciju sa bazom, dodati vrijednosti u tablicu
             naredba (insert), zatvoriti konekciju s bazom*/
+            DatabaseConnection newConnection = new DatabaseConnection();
+            newConnection.Connection();
+
+            SqlCommand comm = new SqlCommand();
+            comm.Connection = DatabaseConnection.conn;
+
+            comm.CommandText = "insert into Artikli values (@Naziv, @Cijena, @Kolicina, @ID_vrsta_artikla)";
+            comm.Parameters.AddWithValue("Naziv", txtNaziv);
+            comm.Parameters.AddWithValue("Cijena", txtCijena);
+            comm.Parameters.AddWithValue("Kolicina", txtKolicina);
+            comm.Parameters.AddWithValue("ID_vrsta_artikla", cmboxTipArtikla);
+
+            comm.ExecuteNonQuery();
+            MessageBox.Show(cmboxTipArtikla.Text + "  dodano." + Environment.NewLine +
+                 txtNaziv.Text + " dodano." + Environment.NewLine + txtCijena.Text +
+                 " dodano." + Environment.NewLine + txtKolicina.Text + " dodano.", "Dodano");
+
+            DatabaseConnection.conn.Close();
         }
     }
 }
